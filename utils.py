e24aa606dad025078c2516ce7c77c44772d63242
cavaliersanstete <luc4spiquet@gmail.com>
2025-03-16 22:28:39 +0100

    data exploration:visualize bpm on a day

---
+++ utils.py
@@ -0,0 +1,29 @@
+import json
+
+def display_json(api_call, output):
+    """Format API output for better readability."""
+
+    dashed = "-" * 20
+    header = f"{dashed} {api_call} {dashed}"
+    footer = "-" * len(header)
+
+    print(header)
+
+    if isinstance(output, (int, str, dict, list)):
+        print(json.dumps(output, indent=4))
+    else:
+        print(output)
+
+    print(footer)
+
+
+def display_text(output):
+    """Format API output for better readability."""
+
+    dashed = "-" * 60
+    header = f"{dashed}"
+    footer = "-" * len(header)
+
+    print(header)
+    print(json.dumps(output, indent=4))
+    print(footer)
